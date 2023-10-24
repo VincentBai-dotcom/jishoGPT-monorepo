f90db608e3bd1713a9e916ea13fc2aad690e6bf5
VincentBai-dotcom <butcheryyy@gmail.com>
2023-10-23 23:05:16 -0400

    home page refactory

--- src/app/[Lang]/page.tsx
+++ src/app/[Lang]/page.tsx
@@ -12,17 +12,21 @@ export default async function Home({ params }: { params: { lang: Locale } }) {
         alt="Cartoon graduates jump with happiness"
         layout="fill"
       /> */}
-
-      <div className=" max-w-screen-md">
-        <h1 className="mb-5 text-5xl font-bold text-neutral-content">
-          Hello there
-        </h1>
-        <p className="mb-5 text-neutral-content">
-          Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-          excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
-          id nisi.
-        </p>
-        <SearchBar />
+      <div className="flex min-h-max justify-center items-start relative py-12">
+        <Image
+          src={homeBackground}
+          alt="homeBackground"
+          layout="fill"
+          objectFit="cover"
+          className=" opacity-50 overflow-hidden "
+        />
+        <div className="max-w-screen-md text-center z-10">
+          <h1 className="mb-5 text-5xl font-bold text-neutral">JishoGPT</h1>
+          <p className="mb-5 text-neutral">
+            Get the most informative definitions for Japanese vocabulary
+          </p>
+          <SearchBar />
+        </div>
       </div>
     </main>
   );
