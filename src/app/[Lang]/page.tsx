3b998743bee98118367be99f6bf38707223fa340
VincentBai-dotcom <butcheryyy@gmail.com>
2023-10-24 20:41:52 -0400

    homescreen change

--- src/app/[Lang]/page.tsx
+++ src/app/[Lang]/page.tsx
@@ -1,31 +1,27 @@
-import Image from "next/image";
 import { Locale } from "../../../i18n.config";
 import { getDictionary } from "@/lib/dictionaries";
 import SearchBar from "@/components/searchBar";
 import homeBackground from "/public/homeBackground.jpg";
+
 export default async function Home({ params }: { params: { lang: Locale } }) {
   const dict = await getDictionary(params.lang);
   return (
     <main className="">
-      {/* <Image
-        src={homeBackground}
-        alt="Cartoon graduates jump with happiness"
-        layout="fill"
-      /> */}
-      <div className="flex justify-center items-start relative py-12">
-        <Image
-          src={homeBackground}
-          alt="homeBackground"
-          layout="fill"
-          objectFit="cover"
-          className=" opacity-60 overflow-hidden "
-        />
-        <div className="max-w-screen-md text-center z-10">
-          <h1 className="mb-5 text-5xl font-bold text-neutral">JishoGPT</h1>
-          <p className="mb-5 text-neutral">
-            Get the most informative definitions for Japanese vocabulary
-          </p>
-          <SearchBar />
+      <div
+        className="hero min-h-screen"
+        style={{ backgroundImage: `url(${homeBackground.src})` }}
+      >
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center ">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold text-neutral-content">
+              JishoGPT
+            </h1>
+            <p className="mb-5 text-neutral-content">
+              Get the most informative definitions for Japanese vocabulary
+            </p>
+            <SearchBar />
+          </div>
         </div>
       </div>
     </main>
