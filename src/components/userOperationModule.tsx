bc13d24678cef6daae2539ebfd42742e7342e18f
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-02 00:44:43 -0400

    fixing set-cookie

--- src/components/userOperationModule.tsx
+++ src/components/userOperationModule.tsx
@@ -1,10 +1,23 @@
+import { cookies } from "next/headers";
 import { isAuthenticated } from "@/lib/serverFunctions";
 import SignInModule from "./signInModule";
 import RegistrationModule from "./registrationModule";
 
-export default async function UserOperationModule() {
-  const response = await isAuthenticated();
+async function getData() {
+  const response = await fetch("http://localhost:8000/auth/is-authenticated", {
+    method: "POST",
+    headers: {
+      mode: "cors",
+      Cookie: cookies().toString(),
+    },
+    credentials: "include",
+  });
+  console.log(response.status);
+  return response;
+}
 
+export default async function UserOperationModule() {
+  const response = await getData();
   return (
     <>
       {(function () {
