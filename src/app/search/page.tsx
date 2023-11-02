3cc3d950d6b79812c10e33e0c8a6bb38f2fc8b59
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-02 18:47:09 -0400

    clean everything

--- src/app/search/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Locale } from "../../../i18n.config";
-import { getDictionary } from "@/lib/dictionaries";
-import SearchBar from "@/components/searchBar";
-import homeBackground from "/public/homeBackground.jpg";
-
-export default async function Home() {
-  return <main className=" bg-base-300 min-h-screen"></main>;
-}
