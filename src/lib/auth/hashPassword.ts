778bcac5826455e8cc3ef8852bf36affd3034c64
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-06 12:29:28 -0500

    hashpassword

--- src/lib/auth/hashPassword.ts
+++ src/lib/auth/hashPassword.ts
@@ -1,1 +1,10 @@
-export const hashWithSalt = (salt: string, password: string): string => {};
+import crypto from "crypto";
+
+export const hashPassword = (password: string, salt: string): string => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(process.env.SECRET || "LOCAL_SECRET")
+    .digest("hex");
+};
+
+export default hashPassword;
