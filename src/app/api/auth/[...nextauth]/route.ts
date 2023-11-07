e7e9c0c52547aad4bf7859a68d9105c80c4367ed
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-07 17:46:46 -0500

    nextauth configuration

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,50 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "../../../../../models/User";
+import connectToDB from "@/lib/db";
+import hashPassword from "@/lib/auth/hashPassword";
+
+const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        emailOrUsername: { label: "EmailOrUsername", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.emailOrUsername || !credentials?.password) {
+          return null;
+        }
+        const { emailOrUsername, password } = credentials;
+        try {
+          await connectToDB();
+          const user = await User.findOne({
+            $or: [{ email: emailOrUsername }, { username: emailOrUsername }],
+          }).select("+authInfo.salt +authInfo.password");
+
+          if (!user) {
+            return null;
+          }
+
+          const expectedPassword = hashPassword(password, user.authInfo.salt);
+
+          if (expectedPassword !== user.authInfo.salt) {
+            return null;
+          }
+          return user;
+        } catch (err) {
+          console.log("### Authorization failed");
+          console.log(err);
+        }
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/auth/signin",
+  },
+};
--- src/lib/auth/hashPassword.ts
+++ src/lib/auth/hashPassword.ts
@@ -3,7 +3,7 @@ import crypto from "crypto";
 export const hashPassword = (password: string, salt: string): string => {
   return crypto
     .createHmac("sha256", [salt, password].join("/"))
-    .update(process.env.SECRET || "LOCAL_SECRET")
+    .update(process.env.HASH_SECRET || "LOCAL_SECRET")
     .digest("hex");
 };
 
