b5762980e2a4038d42f4c262f021dc0c59242eed
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-10 11:24:18 -0500

    setup openai and some helper function

---
+++ src/lib/openai/openaiServices.ts
@@ -0,0 +1,23 @@
+import OpenAI from "openai";
+import { Prompts } from "./prompts";
+
+const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
+export const generateWordDescription = async (word: string) => {
+  const description = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content: Prompts.wordDescriptionSystemPrompy,
+      },
+      {
+        role: "user",
+        content: word,
+      },
+    ],
+    temperature: 0.2,
+  });
+
+  return description;
+};
