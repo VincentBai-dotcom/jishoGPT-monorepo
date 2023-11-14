793c150e6e0c4f1cd66dee77e00fea296677983f
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-13 21:11:59 -0500

    fetching word id info

--- src/app/dict/word/[wordID]/page.tsx
+++ src/app/dict/word/[wordID]/page.tsx
@@ -1,15 +1,15 @@
-"use client";
-import { useEffect, useState } from "react";
 import { notFound } from "next/navigation";
+import { IWordEntry } from "../../../../../models/WordEntry";
 
-async function getWordEntryInfo(searchString: string) {
+async function getWordEntryInfo(wordID: string) {
   const res = await fetch(
-    process.env.NEXT_PUBLIC_API_PATH + "/api/dict/search",
-    {
-      method: "POST",
-      body: JSON.stringify({
-        searchString: searchString,
+    process.env.NEXT_PUBLIC_API_PATH +
+      "/api/word/get?" +
+      new URLSearchParams({
+        wordID,
       }),
+    {
+      method: "GET",
     }
   );
 
@@ -20,12 +20,13 @@ async function getWordEntryInfo(searchString: string) {
   }
 }
 
-export default function Page({ params }: { params: { wordID: string } }) {
-  const wordEntry = getWordEntryInfo(params.wordID);
+export default async function Page({ params }: { params: { wordID: string } }) {
+  const wordEntry: IWordEntry = await getWordEntryInfo(params.wordID);
 
   return (
     <article className="prose lg:prose-xl">
-      <h1></h1>
+      <h1>{wordEntry.word}</h1>
+      <h2>{wordEntry.pronunciation}</h2>
     </article>
   );
 }
