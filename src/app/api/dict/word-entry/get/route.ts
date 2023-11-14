900416b3e4b2b6ceb0723db5929a4187cb58fe07
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-13 21:06:41 -0500

    add word getter api

---
+++ src/app/api/dict/word-entry/get/route.ts
@@ -0,0 +1,27 @@
+import connectToDB from "@/lib/db";
+import WordEntry, { IWordEntry } from "../../../../../../models/WordEntry";
+import { type NextRequest } from "next/server";
+import { Errors } from "../../../../../../errors";
+
+export async function GET(req: NextRequest) {
+  try {
+    await connectToDB();
+    console.log("### Fetching word info from database");
+    const wordId = req.nextUrl.searchParams.get("wordID");
+    const wordEntry = await WordEntry.findById<IWordEntry>(wordId);
+    if (!wordEntry) {
+      console.log("Word does not exist");
+      return Response.json(Errors.wordEntryDoesNotExistError, {
+        status: 400,
+      });
+    }
+    console.log("Word found in database");
+    return Response.json(wordEntry);
+  } catch (err) {
+    console.log("fetching failed");
+    console.log(err);
+    return Response.json(err, {
+      status: 400,
+    });
+  }
+}
--- src/app/dict/word/[wordID]/page.tsx
+++ src/app/dict/word/[wordID]/page.tsx
@@ -1,8 +1,28 @@
 "use client";
-
 import { useEffect, useState } from "react";
+import { notFound } from "next/navigation";
 
-export default function Page({ params }: { params: { wordID: number } }) {
+async function getWordEntryInfo(searchString: string) {
+  const res = await fetch(
+    process.env.NEXT_PUBLIC_API_PATH + "/api/dict/search",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        searchString: searchString,
+      }),
+    }
+  );
+
+  if (res.ok) {
+    return res.json();
+  } else {
+    return notFound();
+  }
+}
+
+export default function Page({ params }: { params: { wordID: string } }) {
+  const wordEntry = getWordEntryInfo(params.wordID);
+
   return (
     <article className="prose lg:prose-xl">
       <h1></h1>
