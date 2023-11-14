45c5a1b084712f19a564035980c868c8ea50f78c
VincentBai-dotcom <butcheryyy@gmail.com>
2023-11-13 23:18:30 -0500

    add typography layout

--- src/app/dict/word/[wordID]/page.tsx
+++ src/app/dict/word/[wordID]/page.tsx
@@ -23,10 +23,12 @@ export default async function Page({ params }: { params: { wordID: string } }) {
   const wordEntry: IWordEntry = await getWordEntryInfo(params.wordID);
 
   return (
-    <div>
-      <article className="prose lg:prose-xl ">
-        <h1>{wordEntry.word}</h1>
-        <h2>{wordEntry.pronunciation}</h2>
+    <div className="col-start-4 col-end-10">
+      <article className="prose lg:prose-lg mt-4 max-w-none">
+        <h3 style={{ margin: "0" }}>{wordEntry.pronunciation}</h3>
+        <h1 style={{ margin: "0" }}>{wordEntry.word}</h1>
+        <div className="divider"></div>
+        <h3 style={{ marginTop: "0" }}>Description</h3>
       </article>
     </div>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <AuthProvider>
           {
-            <div className=" bg-base-100 ">
+            <div className="bg-base-100 ">
               <NavBar />
               {children}
             </div>
