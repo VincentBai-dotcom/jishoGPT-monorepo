af274de48b1503936011aef08fa497fd4a67085b
VincentBai-dotcom <butcheryyy@gmail.com>
2023-12-01 00:20:47 -0500

    temp pricing page

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,13 @@
+import Script from "next/script";
+
+export default async function Page() {
+  return (
+    <>
+      <Script src="https://js.stripe.com/v3/pricing-table.js" />
+      <stripe-pricing-table
+        pricing-table-id="prctbl_1OINT0GsQl5mNBHeSQHD2U89"
+        publishable-key="pk_live_51OHeyzGsQl5mNBHehd7kJKK2yu25ZzWFKeuLPlIN2fKBpZhTrrFuK2EVIYEgjCdOzFzeg4vQi57G7Mb0kF5szPoy00zoUjntJL"
+      ></stripe-pricing-table>
+    </>
+  );
+}
