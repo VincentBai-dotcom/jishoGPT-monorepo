061b1303330a38da36baf4f8e767ed5cf9047554
VincentBai-dotcom <butcheryyy@gmail.com>
2023-12-03 00:01:14 -0500

    allow passing data to PriceCard

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -2,14 +2,5 @@ import PriceCard from "@/components/payment/PriceCard";
 import Script from "next/script";
 
 export default async function Page() {
-  return (
-    <div className="min-h-screen">
-      <Script src="https://js.stripe.com/v3/pricing-table.js" />
-      <stripe-pricing-table
-        pricing-table-id="prctbl_1OINT0GsQl5mNBHeSQHD2U89"
-        publishable-key="pk_live_51OHeyzGsQl5mNBHehd7kJKK2yu25ZzWFKeuLPlIN2fKBpZhTrrFuK2EVIYEgjCdOzFzeg4vQi57G7Mb0kF5szPoy00zoUjntJL"
-      ></stripe-pricing-table>
-      <PriceCard />
-    </div>
-  );
+  return <div className="min-h-screen">{/* <PriceCard /> */}</div>;
 }
--- src/components/payment/PriceCard.tsx
+++ src/components/payment/PriceCard.tsx
@@ -1,21 +1,45 @@
-export default function PriceCard() {
+export default function PriceCard({
+  params,
+}: {
+  params: {
+    productName: string;
+    price: string;
+    description: string;
+    details: [string];
+    onSubmit: () => void;
+    showUnit?: boolean;
+    buttonText?: string;
+    isButtonDisabled?: boolean;
+  };
+}) {
   return (
     <div className="rounded-2xl relative flex flex-col w-96 bg-base-100 shadow-xl gap-5 p-8 text-base">
-      <h1 className="text-2xl font-semibold mb-3">{"Basic Tier"}</h1>
+      <h1 className="text-2xl font-semibold mb-3">{params.productName}</h1>
       <div className="flex items-center gap-2">
-        <span className="text-5xl font-extrabold">{"$10"}</span>
-        <span className="text">
-          <div className="flex flex-col">
-            <p>per</p>
-            <p>month</p>
-          </div>
-        </span>
+        <span className="text-5xl font-extrabold">{params.price}</span>
+        {params.showUnit === undefined || params.showUnit ? (
+          <span className="text">
+            <div className="flex flex-col">
+              <p>per</p>
+              <p>month</p>
+            </div>
+          </span>
+        ) : (
+          <></>
+        )}
       </div>
-      <button className="btn btn-primary w-full text-base">Subscribe</button>
-      <h2 className="text-lg font-semibold">Try out JishoGPT for free</h2>
+      <button
+        className={`btn btn-primary w-full text-base ${
+          params.isButtonDisabled === false ? "btn-disabled" : ""
+        }`}
+      >
+        {params.buttonText || "Subscribe"}
+      </button>
+      <h2 className="text-lg font-semibold">{params.description}</h2>
       <ul className="flex flex-col gap-0 text-base font-medium list-disc">
-        <li>30 Search per day</li>
-        <li>30 Search per day</li>
+        {params.details.map((detail, index) => {
+          return <li key={index}>{detail}</li>;
+        })}
       </ul>
     </div>
   );
