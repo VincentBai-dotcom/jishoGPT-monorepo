ea7935ffb6d3fd7af97f6690c15aea38db318db8
VincentBai-dotcom <butcheryyy@gmail.com>
2023-12-09 20:01:17 -0500

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import { AuthProvider } from "@/components/AuthProvider";
 import NavBar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
           }
         </AuthProvider>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
